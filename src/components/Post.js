1b642333050f1a26ac1ae0254f5141d04b192282
Denis <denis.bortnikov17@gmail.com>
2021-09-22 14:37:11 +0300

    [F2] Remove files and add new components

---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+};
